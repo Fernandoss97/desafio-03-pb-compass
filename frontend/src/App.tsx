cd96825e4250ef0ecf9906def06ecc7d2b8dba74
Fernandoss97 <fernandonascimento@alunos.utfpr.edu.br>
2024-08-27 17:32:03 -0300

    feat: create book settings component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import SliderFilter from "./components/sliderFilter/SliderFilter";
 import CategoryFilter from "./components/categoryFilter/CategoryFilter";
 import DestFilter from "./components/destFilter/DestFilter";
 import ReviewFilter from "./components/reviewFilter/ReviewFilter";
+import BookSettings from "./components/bookSettings/BookSettings";
 
 function App() {
   return (
@@ -26,11 +27,7 @@ function App() {
           justifyContent: "center",
           padding: "50px",
         }}
-      >
-        {/* <CategoryFilter
-          options={["teste", "Show", "Adventure", "Beach", "Food", "Honeymoon", "Hiking"]}
-        /> */}
-      </div>
+      ></div>
     </>
   );
 }
---
+++ frontend/src/components/bookSettings/BookSettings.tsx
@@ -0,0 +1,86 @@
+import styles from "./bookSettings.module.css";
+import { useState } from "react";
+
+const BookSettings = () => {
+  let [adults, setAdults] = useState(0);
+  let [kids, setKids] = useState(0);
+  let [children, setChildren] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.ct_price}>
+        <span>$104</span>
+        <p>/ per person</p>
+      </div>
+      <div className={styles.ct_time}>
+        <label className={styles.ct_label}>
+          Date
+          <input type="date" placeholder="Choose date" />
+        </label>
+        <label className={styles.ct_label}>
+          Time
+          <select id="">
+            <option value="teste">Teste</option>
+            <option value="teste">Teste</option>
+            <option value="teste">Teste</option>
+          </select>
+        </label>
+      </div>
+      <div className={styles.ct_ticket}>
+        <p className={styles.title}>Ticket</p>
+        <div className={styles.ct_qtd}>
+          <p>Adults (18+ years)</p>
+          <div className={styles.qtd_picker}>
+            <button
+              onClick={e => (adults === 0 ? setAdults(0) : setAdults(a => a - 1))}
+              className={styles.bt_qtd}
+            >
+              -
+            </button>
+            <span>{adults}</span>
+            <button onClick={e => setAdults(a => a + 1)} className={styles.bt_qtd}>
+              +
+            </button>
+          </div>
+        </div>
+        <div className={styles.ct_qtd}>
+          <p>Kids (12+ years)</p>
+          <div className={styles.qtd_picker}>
+            <button
+              onClick={e => (kids === 0 ? setKids(0) : setKids(a => a - 1))}
+              className={styles.bt_qtd}
+            >
+              -
+            </button>
+            <span>{kids}</span>
+            <button onClick={e => setKids(a => a + 1)} className={styles.bt_qtd}>
+              +
+            </button>
+          </div>
+        </div>
+        <div className={styles.ct_qtd}>
+          <p>Children (3+ years)</p>
+          <div className={styles.qtd_picker}>
+            <button
+              onClick={e => (children === 0 ? setChildren(0) : setChildren(a => a - 1))}
+              className={styles.bt_qtd}
+            >
+              -
+            </button>
+            <span>{children}</span>
+            <button onClick={e => setChildren(a => a + 1)} className={styles.bt_qtd}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.ct_total}>
+        <p>Total</p>
+        <span>$104</span>
+      </div>
+      <button className={styles.bt_book}>Book Now</button>
+    </div>
+  );
+};
+
+export default BookSettings;
